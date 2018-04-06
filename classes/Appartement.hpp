ac6b13f5399ddfa73b0ad4fd67cb0d4cfa9592be
Mathieuu97 <reviron.mathieu.33@gmail.com>
2018-04-06 12:00:25 +0200

    Appartement.hpp

--- classes/Appartement.hpp
+++ classes/Appartement.hpp
@@ -10,5 +10,23 @@
 #define Appartement_hpp
 
 #include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+class Appartement : public Bien {
+protected:
+    int nb_pieces;
+    int nb_etage;
+    bool garage;
+    bool cave;
+    bool balcon;
+    int nb_total_appart;
+public:
+    Appartement();
+    Appartement(int nb_pieces, int nb_etages, bool garage, bool cave, bool balcon, int nb_total_appart);
+    ~Appartement();
+};
+
 
 #endif /* Appartement_hpp */
